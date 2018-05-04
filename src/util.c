161239881e7f0aceaf9722e6b13bf08e1eda8417
Roger Wehage <raw915@gmail.com>
2018-05-04 14:20:48 -0500

    finish functions in util.c and util.h

--- src/util.c
+++ src/util.c
@@ -1,4 +1,4 @@
-#include <math.h>
+#include <util.h>
 
 /*!
 @brief Normalize double precision angle in degrees
@@ -288,4 +288,102 @@ inline void ksl_axpyf(const int n, const float a, const float* restrict x,
   for(int i = 0; i < n; i++) {
     y[i] += a * x[i];
   }
+}
+
+/*!
+@brief used to determine if two double arrays are close to each other
+within an error tolerance of 1e-6
+
+@param count [in] number of entries in array
+@param a1 [in] first double array
+@param a2 [in] second double array
+@returns true if all pairs are close, false at the first pair not close
+*/
+bool ksl_allclose(const int count, const double* restrict a1,
+                  const double* restrict a2) {
+
+  for(int i = 0; i < count; i++) {
+    double scaleFactor = fmax(fabs(a1[i]), 1.0);
+    if(fabs(a1[i] - a2[i]) / scaleFactor > 1e-6) {
+      fprintf(stdout, "match failed at index: %d\n", i);
+      return false;
+    }
+  }
+  return true;
+}
+
+/*!
+@brief used to determine if two single precision arrays are close to each other
+within an error tolerance of 1e-6
+
+@param count [in] number of entries in array
+@param a1 [in] first float array
+@param a2 [in] second float array
+@returns true if all pairs are close, false at the first pair not close
+*/
+bool ksl_allclosef(const int count, const float* restrict a1,
+                   const float* restrict a2) {
+
+  for(int i = 0; i < count; i++) {
+    double scaleFactor = fmax(fabs(a1[i]), 1.0);
+    if(fabs(a1[i] - a2[i]) / scaleFactor > 1e-6) {
+      fprintf(stdout, "match failed at index: %d\n", i);
+      return false;
+    }
+  }
+  return true;
+}
+
+/*!
+@brief linear interpolation
+
+@param count [in] number of entries to interpolate
+@param p [in] interpolant value, ranges from 0 to 1
+@param y1i [in] value of function at p = 0
+@param y2i [in] value of function at p = 1
+@param yo [out] interpolated data returned in yo
+
+*/
+void ksl_arraylerp(const int count, const double p, const double* restrict y1i,
+                   const double* restrict y2i, double* restrict yo) {
+
+  if(p < 1e-9) {
+    memcpy(yo, y1i, count * sizeof(double));
+    return;
+  } else if(p >= 1.0) {
+    memcpy(yo, y2i, count * sizeof(double));
+    return;
+  }
+
+  for(int i = 0; i < count; i++) {
+    yo[i] = y1i[i] + p * (y2i[i] - y1i[i]);
+  }
+  return;
+}
+
+/*!
+@brief linear interpolation
+
+@param count [in] number of entries to interpolate
+@param p [in] interpolant value, ranges from 0 to 1
+@param y1i [in] value of function at p = 0
+@param y2i [in] value of function at p = 1
+@param yo [out] interpolated data returned in yo
+
+*/
+void ksl_arraylerpf(const int count, const float p, const float* restrict y1i,
+                    const float* restrict y2i, float* restrict yo) {
+
+  if(p < 1e-9) {
+    memcpy(yo, y1i, count * sizeof(float));
+    return;
+  } else if(p >= 1.0) {
+    memcpy(yo, y2i, count * sizeof(float));
+    return;
+  }
+
+  for(int i = 0; i < count; i++) {
+    yo[i] = y1i[i] + p * (y2i[i] - y1i[i]);
+  }
+  return;
 }
