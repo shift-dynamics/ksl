fdca0a49018d6d6cc79c5bd5c236ac72a92bab65
Kristopher Wehage <kriswehage@gmail.com>
2021-06-01 10:53:49 -0700

    Build system updates

--- cmake/CodeCoverage.cmake
+++ cmake/CodeCoverage.cmake
@@ -75,7 +75,7 @@ find_program( GCOV_PATH gcov )
 find_program( LCOV_PATH  NAMES lcov lcov.bat lcov.exe lcov.perl)
 find_program( GENHTML_PATH NAMES genhtml genhtml.perl genhtml.bat )
 find_program( GCOVR_PATH gcovr PATHS ${CMAKE_SOURCE_DIR}/scripts/test)
-find_package(Python COMPONENTS Interpreter)
+find_package(Python3 COMPONENTS Interpreter)
 
 if(NOT GCOV_PATH)
     message(FATAL_ERROR "gcov not found! Aborting...")
